574599392e849e7ebe189f7549e207b160d38c44
Абдукадиров Фархад <farhadabdukadirov007@gmail.com>
2023-02-06 02:24:35 +0600

    creat code

--- Labka/Labka3/zadach3.py
+++ Labka/Labka3/zadach3.py
@@ -1,5 +1,10 @@
-from random import randint
+"""from random import randint
 
 N = 10
 A = [randint(1, 20) for i in range(N)]
 print(A)
+"""
+a = [10, 20, 30, 40, 50, 60]
+
+for index, value in enumerate(a):
+    print(index, ":", value)
